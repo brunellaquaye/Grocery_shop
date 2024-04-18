43d1fcf37f5c16df99a2be7fe3c5e0485ceb9960
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-04-18 18:38:06 +0000

    feat: Increased padding  on home page to enhance layout

--- tryout.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector("button.menu_icon");
-const menu = document.querySelector(".menu-item");
-btn.addEventListener("click",()=>{
-    menu.classList.toggle("hidden");
-})
